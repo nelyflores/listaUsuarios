14691e67e77365f2cfb9f81e62a1b76ff030fb75
nelyflores <nely.afloresr@gmail.com>
2017-12-01 15:13:41 -0600

    realiza la tarjeta detalles

--- js/modal.js
+++ js/modal.js
@@ -7,37 +7,34 @@ function mostrarDetalles(){
  var telefono=localStorage.getItem("Telefono");
  var nombre=localStorage.getItem("Nombre");
  var username=localStorage.getItem("Username");
+ var modal =`
+  <div class="modal">
+    <div class="tarjeta__detalle">
+      <div class="tarjeta__header">
+        <img src="${imagen}" class="modal__img">
+      <div class="tarjeta__informacion">
+        <h2 class="datos_api">
+          <span>${nombre}</span> <br>
+          <span>${username}</span><br>
+        </h2>
+      </div> 
+      </div>
+      <div class="content__modal">
+        <span>${direccion}</span><br>
+        <span>${correo}</span><br>
+        <span>Tel: ${telefono}</span>
+        <p>YOU AND LULA</p>
+        <p>Text Messages<span>10k+</span></p><hr>
+        <p>Photo & Images <span> 562 </span></p><hr>
+      </div>
+      <div class="modal__boton"><i  onclick="regresar()"class="fa fa-chevron-left" aria-hidden="true"></i></div>
+    </div>
+  </div>`;
 
-   
-
-     var modal =`<div class="modal">
-   <div class="profile__card">
-  <div class="header__modal">
-  <img src="${imagen}" class="modal__img">
-    <h2 class="datos_api">
-    <span>${nombre}</span> <br>
-    <span>${username}</span><br>
-    <span>${direccion}</span><br>
-    <span>${correo}</span><br>
-    <span>${telefono}</span>
-    </h2>
-  </div>
-  </div>
-  <div class="content__modal">
-  <p>YOU AND LULA</p>
-  <h2>TextMessages<span>10k+</span></h2>
-  <h2>Photo & Images <span>562</span></h2>
-  <h2>Videos <span>13</span></h2>
-  <h2>Emoticons <span>1539</span></h2>
-    
-  </div>
-  <div class="modal__boton">x</div>
-</div>
-</div>`;
-
-    contentModal.insertAdjacentHTML('beforeEnd',modal)
-    
-   
-   console.log(valor);
-   }
-   mostrarDetalles();
+ contentModal.insertAdjacentHTML('beforeEnd',modal)
+}
+function regresar(){
+  var regresarInicio="index.html"
+  window.location=regresarInicio;
+}
+mostrarDetalles();
--- js/script.js
+++ js/script.js
@@ -15,21 +15,19 @@ const getConnection = () =>{
 const drawUser = e =>{
   const hero=`
   <div id="${e.id.value}" class="galeria__img content-img" onclick="mostrarDetalles(this)" >
-  <img src="${e.picture.large}" data-imagen= "${e.picture.large}">
+    <img src="${e.picture.large}" data-imagen= "${e.picture.large}">
   <section class="portafolio-text" data-correo="${e.email}" data-direccion="${e.location.street}" data-telefono"=${e.phone}" >
-   <h2 data-name=${e.name.first}><i class="fa fa-circle-o" aria-hidden="true"></i>${e.name.first}</h2>
-  <p data-username=${e.login.username}>${e.login.username}</p>
+    <h2 data-name=${e.name.first}><i class="fa fa-circle-o" aria-hidden="true"></i>${e.name.first}</h2>
+    <p data-username=${e.login.username}>${e.login.username}</p>
   </section>
   </div>
   `;
-  
   content.insertAdjacentHTML('beforeEnd',hero);
 };
 
 function mostrarDetalles(caja){
   var modalpag="modal.html";
   window.location=modalpag;
-
     var nombre = caja.childNodes;
     var valor = nombre[1].attributes[1].nodeValue;
     var valorcorreo=nombre[3].attributes[1].nodeValue;
@@ -37,17 +35,14 @@ function mostrarDetalles(caja){
     var valorTelefono=nombre[3].attributes[3].nodeValue;
     var valornombre=nombre[3].childNodes[1].attributes[0].nodeValue;
     var valorusername=nombre[3].childNodes[3].attributes[0].nodeValue;
-
     localStorage.setItem("Imagen", valor);
     localStorage.setItem("Correo", valorcorreo);
     localStorage.setItem("Direccion", valordireccion);
     localStorage.setItem("Telefono", valorTelefono);
     localStorage.setItem("Nombre", valornombre);
     localStorage.setItem("Username", valorusername);
-
-   
-   console.log(valorusername);
-   }
+  console.log(valorusername);
+}
 
 
 getConnection()
